b3331ced8864689d65b338c655dc94deea4d0f79
Nate Gervenak <gervenaknate@gmail.com>
2022-02-23 13:08:29 -0600

    premajor refactor

--- assets/js/sfext-nav-scripts.js
+++ assets/js/sfext-nav-scripts.js
@@ -13,13 +13,28 @@ const sfextToggleDisplayBtns = document.querySelectorAll('.sfext-menu-item');
 
 sfextToggleDisplayBtns.forEach(function(item) {
   item.addEventListener("click", () => {
-    let controlId = item.id;
-    let showId = controlId.replace('cntrl', 'show');
-    document.getElementById(showId).classList.toggle(sfextDisplayOpen);
-    item.classList.toggle(sfextDisplayOpen);
+    sfextMenuToggleControl(item);
+    // let controlId = item.id;
+    // let showId = controlId.replace('cntrl', 'show');
+    // // close the open instance
+    // document.getElementById(showId).classList.toggle(sfextDisplayOpen);
+    // item.classList.toggle(sfextDisplayOpen);
   })
 });
 
-// if there is one open and a click on any other element besides itself it needs to close
-// create an open state
-// on keydown event or even tap event
+
+function sfextMenuToggleControl(item) {
+  let controlId = item.id;
+  let showId = controlId.replace('cntrl', 'show');
+  // close the open instance
+  document.getElementById(showId).classList.tog
+}
+
+// http://jsfiddle.net/5mhqrhwk/3/
+
+// if there is one open and a click on any other element besides the active open it must close
+// focus must be set to the first focusable element within the menu
+// if another menu option is closed we need to close the menu first
+// Ideally I'd bring this fully into a Javascript object to be called with sfextMegaMenu.funciton
+// Read some more code examples about making this an object
+
