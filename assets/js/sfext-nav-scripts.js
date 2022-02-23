0175f76b07d7f5cac05152c84c42ba2f4ab32869
Nate Gervenak <gervenaknate@gmail.com>
2022-02-22 22:02:54 -0600

    refactor better naming

--- assets/js/sfext-nav-scripts.js
+++ assets/js/sfext-nav-scripts.js
@@ -1,21 +1,25 @@
 /*
 * Toggle the visibility of non-nested elements with a control and show
 * Requires: 
-* .sfext-menu-item: universal class used to identify a control in the dom
-* .sfext-open: universal class to apply an active state
+* .sfext-menu-item: used to create an array of sfextToggleDisplayBtns for event to listen on
+* .sfext-open: the universal css class used to turn the display on, not unique
 * #sfext-cntrl-{identifier} unique control id in dom
 * #sfext-show-{identifier} unique show id in dom
 * 
 */
 
-const sfextActive = "sfext-open"; // apply this class to show
-const sfextToggleMenu = document.querySelectorAll('.sfext-menu-item');
+const sfextDisplayOpen = "sfext-open"; // apply this class to show
+const sfextToggleDisplayBtns = document.querySelectorAll('.sfext-menu-item');
 
-sfextToggleMenu.forEach(function(item) {
+sfextToggleDisplayBtns.forEach(function(item) {
   item.addEventListener("click", () => {
     let controlId = item.id;
     let showId = controlId.replace('cntrl', 'show');
-    document.getElementById(showId).classList.toggle(sfextActive);
-    item.classList.toggle(sfextActive);
+    document.getElementById(showId).classList.toggle(sfextDisplayOpen);
+    item.classList.toggle(sfextDisplayOpen);
   })
 });
+
+// if there is one open and a click on any other element besides itself it needs to close
+// create an open state
+// on keydown event or even tap event
